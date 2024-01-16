71d4d2d161835226ca43e2a2431198df72f88c36
Michael <fatlycka@gmail.com>
2024-01-16 20:34:56 +0300

    add checkboxes that allows user to choose pass symbols & change some styles

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const generatePassButton = document.querySelector('.generate-btn');
+const passWindow = document.querySelector('.pass-window');
 const passReveal = document.querySelector('.password-reveal');
 const passLength = document.querySelector('#how-num');
+const checkBoxInputs = document.querySelectorAll('input[type="checkbox"');
 
 generatePassButton.addEventListener('click', generatePassWord);
 
@@ -9,28 +11,41 @@ let passValue = Math.floor(passLength.value);
 
 
 function generatePassWord() {
+    let boxesState = [...checkBoxInputs].map(el => el.checked);
+
     let arr = [];
-let min = 48;
-let max = 57;
-
-let minchar = 65;
-let maxchar = 90;
-
-let minlowchar = 97;
-let maxlowchar = 122;
+    let checkboxArr = [];
+  // 0-9
+    let min = 48;
+    let max = 57;
+  // A-Z
+    let minchar = 65;
+    let maxchar = 90;
+  // a-z
+    let minlowchar = 97;
+    let maxlowchar = 122;
+  // special
+    let specialChars = '!@#$%^&*'.split('').map(el => el.charCodeAt(0));
 //  let unicodeRandomNumber = Math.floor(Math.random() * (max - min + 1) ) + min;
 //  let unicodeRandomNumberTwo = Math.floor(Math.random() * (max - min + 1) ) + min;
 for (let i = 0; i < `${passValue}`; i++){
-    let randNumbers = Math.floor(Math.random() * (max - min + 1) ) + min;
-    let randUpperChar = Math.floor(Math.random() * (maxchar - minchar + 1) ) + minchar;
-    let randLowerChar = Math.floor(Math.random() * (maxlowchar - minlowchar + 1) ) + minlowchar;
+  let randNumbers = Math.floor(Math.random() * (max - min + 1) ) + min;
+  let randUpperChar = Math.floor(Math.random() * (maxchar - minchar + 1) ) + minchar;
+  let randLowerChar = Math.floor(Math.random() * (maxlowchar - minlowchar + 1) ) + minlowchar;
+  let randomSpecialChar = specialChars[Math.floor(Math.random() * specialChars.length)];
+  checkboxArr = [randNumbers, randUpperChar, randLowerChar, randomSpecialChar];
 
-    arr.push(randNumbers, randUpperChar, randLowerChar);
+  for(let k = 0; k < checkboxArr.length; k++){
+    if(boxesState[k]){
+      arr.push(checkboxArr[k]);
+    }
+
+  }
+    
 
     // Swap elements in Array
     // arr.sort(() => Math.random() - 0.5);
-
-
+    
     // Swap elements in Array v2
     for (let i = arr.length - 1; i > 0; i--) {
         let j = Math.floor(Math.random() * (i + 1)); // random index from 0 to i
@@ -45,8 +60,9 @@ let uniArray = arr.map(elem => String.fromCharCode(elem));
 uniArray.length = passLength.value;
 let generatedString = uniArray.join("");
 
-console.log(generatedString);
-passReveal.innerHTML = `${generatedString}`;
+
+passReveal.innerText = `${generatedString}`;
+
 }
 
 // Copy Pass Into Clipboard
